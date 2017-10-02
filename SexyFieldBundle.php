a8d334e45d95a163917e1fca5ff0bd4d3ad374fb
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-02 13:17:57 +0200

    Registered the commands

--- SexyFieldBundle.php
+++ SexyFieldBundle.php
@@ -13,6 +13,7 @@ declare (strict_types=1);
 
 namespace Tardigrades\Bundle\SexyFieldBundle;
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -24,5 +25,35 @@ class SexyFieldBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+    }
+
+    public function registerCommands(Application $application)
+    {
+        $application->addCommands([
+            $this->container->get('section_field.create.application.command'),
+            $this->container->get('section_field.update.application.command'),
+            $this->container->get('section_field.delete.application.command'),
+            $this->container->get('section_field.list.application.command'),
+            $this->container->get('section_field.create.language.command'),
+            $this->container->get('section_field.update.language.command'),
+            $this->container->get('section_field.delete.language.command'),
+            $this->container->get('section_field.list.language.command'),
+            $this->container->get('section_field.install.field.type.command'),
+            $this->container->get('section_field.update.field.type.command'),
+            $this->container->get('section_field.delete.field.type.command'),
+            $this->container->get('section_field.list.field.type.command'),
+            $this->container->get('section_field.create.section.command'),
+            $this->container->get('section_field.update.section.command'),
+            $this->container->get('section_field.restore.section.command'),
+            $this->container->get('section_field.delete.section.command'),
+            $this->container->get('section_field.list.section.command'),
+            $this->container->get('section_field.generate.section.command'),
+            $this->container->get('section_field.create.field.command'),
+            $this->container->get('section_field.update.field.command'),
+            $this->container->get('section_field.delete.field.command'),
+            $this->container->get('section_field.list.field.command')
+        ]);
+
+        return parent::registerCommands($application);
     }
 }
