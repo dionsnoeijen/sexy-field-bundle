cb32bad169ecaf1785c066f573673cdefa237dbf
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-03 09:42:39 +0200

    Added license

--- DependencyInjection/Compiler/XmlDriverCompilerPass.php
+++ DependencyInjection/Compiler/XmlDriverCompilerPass.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the SexyField package.
+ *
+ * (c) Dion Snoeijen <hallo@dionsnoeijen.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
 namespace Tardigrades\Bundle\SexyFieldBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
