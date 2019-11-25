20fe778b8d52c680e3dcbf4f8fb32f2a543a70ae
Dion Snoeijen <hallo@dionsnoeijen.nl>
2019-11-25 11:52:23 +0100

    Updat config to sym-4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,21 +24,15 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('sexy_field');
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->useAttributeAsKey('name')
             ->prototype('array')
-            ->useAttributeAsKey('name')
-            ->prototype('variable')
-            ->end()
-            ->end()
-        ;
+                ->useAttributeAsKey('name')
+                ->prototype('variable')
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
